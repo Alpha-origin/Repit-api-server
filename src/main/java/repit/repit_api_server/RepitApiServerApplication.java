f563da1bc3cda32b6f28b007f5c046ea43977712
kimwuju <ugim60350@gmail.com>
2026-05-06 21:20:31 +0900

    feat: api 서버 파일 구조 정의

---
+++ src/main/java/repit/repit_api_server/RepitApiServerApplication.java
@@ -0,0 +1,13 @@
+package repit.repit_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepitApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepitApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/repit/repit_api_server/RepitApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package repit.repit_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepitApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
