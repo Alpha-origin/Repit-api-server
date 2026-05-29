29a32f428189b90b868d9fbea8cd548560818cfd
kimwuju <ugim60350@gmail.com>
2026-05-29 11:18:06 +0900

    feat: PersonaRepository 추가

---
+++ src/main/java/repit/repit_api_server/domain/userdata/interview/repository/PersonaRepository.java
@@ -0,0 +1,7 @@
+package repit.repit_api_server.domain.userdata.interview.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import repit.repit_api_server.domain.userdata.interview.entity.Persona;
+
+public interface PersonaRepository extends JpaRepository<Persona, Integer> {
+}
