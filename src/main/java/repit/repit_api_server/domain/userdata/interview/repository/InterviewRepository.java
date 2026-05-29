843c0c1bc4c7c1e505b5956e66b563f793670b10
kimwuju <ugim60350@gmail.com>
2026-05-29 19:11:22 +0900

    feat: interviewRepository 추가

---
+++ src/main/java/repit/repit_api_server/domain/userdata/interview/repository/InterviewRepository.java
@@ -0,0 +1,8 @@
+package repit.repit_api_server.domain.userdata.interview.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import repit.repit_api_server.domain.userdata.interview.entity.Interview;
+
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+}
