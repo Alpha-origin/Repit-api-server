5f87ded3594ff30fb743d18d9ceaa75dcc65b744
kimwuju <ugim60350@gmail.com>
2026-05-30 12:01:34 +0900

    feat: Answer Entity 추가

---
+++ src/main/java/repit/repit_api_server/domain/userdata/question/entity/Answer.java
@@ -0,0 +1,43 @@
+package repit.repit_api_server.domain.userdata.question.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import repit.repit_api_server.domain.userdata.interview.entity.Interview;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "interview_answer")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answer_id")
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Interview interview;
+
+    @JoinColumn(nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    private Question question;
+
+    @JoinColumn(nullable = false)
+    private Long userId;
+
+    @Column()
+    private int responseTime;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
