98be638625e63eed102e6a82d23bcd072dfba93f
kimwuju <ugim60350@gmail.com>
2026-06-01 23:32:26 +0900

    fix: createQuestion이면서 save를 안 한 버그 수정

--- src/main/java/repit/repit_api_server/domain/userdata/question/service/QuestionService.java
+++ src/main/java/repit/repit_api_server/domain/userdata/question/service/QuestionService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import repit.repit_api_server.domain.userdata.question.dto.request.QuestionRequest;
 import repit.repit_api_server.domain.userdata.question.dto.response.QuestionResponse;
+import repit.repit_api_server.domain.userdata.question.entity.Question;
 import repit.repit_api_server.domain.userdata.question.repository.QuestionRepository;
 import repit.repit_api_server.global.client.AiServerClient;
 
@@ -14,6 +15,15 @@ public class QuestionService {
     private AiServerClient aiServerClient;
 
     public QuestionResponse createQuestion(String authorization) {
-        return aiServerClient.createQuestion(authorization);
+        QuestionResponse response = aiServerClient.createQuestion(authorization);
+        Question question = Question.builder()
+                .interview(response.getInterview())
+                .question(response.getQuestion())
+                .type(response.getType())
+                .intention(response.getIntention())
+                .content(response.getContent())
+                .build();
+        questionRepository.save(question);
+        return response;
     }
 }
