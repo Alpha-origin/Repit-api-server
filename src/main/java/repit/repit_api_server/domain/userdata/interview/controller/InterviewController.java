2ca034cc680282fcf528d88c27a64fcf1cf1bd52
kimwuju <ugim60350@gmail.com>
2026-06-01 21:50:20 +0900

    feat: saveInterview from InterviewController

---
+++ src/main/java/repit/repit_api_server/domain/userdata/interview/controller/InterviewController.java
@@ -0,0 +1,20 @@
+package repit.repit_api_server.domain.userdata.interview.controller;
+
+import org.springframework.web.bind.annotation.*;
+import repit.repit_api_server.domain.userdata.interview.dto.response.InterviewResponse;
+import repit.repit_api_server.domain.userdata.interview.entity.Persona;
+import repit.repit_api_server.domain.userdata.interview.service.InterviewService;
+import repit.repit_api_server.global.common.ApiResponse;
+
+@RestController
+@RequestMapping("/api/v1/interview")
+public class InterviewController {
+    private InterviewService interviewService;
+
+    @PostMapping("/saveInterview")
+    public ApiResponse<InterviewResponse> saveInterview(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody Persona persona) {
+        return ApiResponse.created(interviewService.saveInterview(authorization, persona));
+    }
+}
