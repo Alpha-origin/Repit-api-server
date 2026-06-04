f9ad8d52498f372f4b5a7ab1fc4005c9a2c0913b
kimwuju <ugim60350@gmail.com>
2026-06-04 20:44:28 +0900

    feat: findAllInterview from InterviewController

--- src/main/java/repit/repit_api_server/domain/userdata/interview/controller/InterviewController.java
+++ src/main/java/repit/repit_api_server/domain/userdata/interview/controller/InterviewController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.*;
 import repit.repit_api_server.domain.userdata.interview.dto.response.InterviewResponse;
 import repit.repit_api_server.domain.userdata.interview.entity.Persona;
 import repit.repit_api_server.domain.userdata.interview.service.InterviewService;
+import repit.repit_api_server.domain.userdata.question.service.AnswerService;
 import repit.repit_api_server.global.common.ApiResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/interview")
 @RequiredArgsConstructor
 public class InterviewController {
     private final InterviewService interviewService;
+    private final AnswerService answerService;
 
     @PostMapping("/createInterview")
     public ApiResponse<InterviewResponse> createInterview(
@@ -26,5 +30,11 @@ public class InterviewController {
             @RequestBody Long interviewId
     ) {
         interviewService.sendUserData(authorization, interviewId);
+    }
+
+    @GetMapping("/findAllInterview")
+    public ApiResponse<List<InterviewResponse>> findAllInterview(
+            @RequestHeader("Authorization") String authorization) {
+        return ApiResponse.success(interviewService.getAllInterviewsByUserId(authorization));
     }
 }
