b436d3cdb5eba9d9259de845fac921a3f380b4e9
kimwuju <ugim60350@gmail.com>
2026-06-04 16:43:35 +0900

    feat:add AnswerResponse

---
+++ src/main/java/repit/repit_api_server/domain/userdata/question/dto/response/AnswerResponse.java
@@ -0,0 +1,24 @@
+package repit.repit_api_server.domain.userdata.question.dto.response;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import repit.repit_api_server.domain.userdata.interview.entity.Interview;
+import repit.repit_api_server.domain.userdata.question.entity.Question;
+
+import java.time.LocalDateTime;
+
+public class AnswerResponse {
+    private Long id;
+
+    private Interview interview;
+
+    private Question question;
+
+    private Long userId;
+
+    private int responseTime;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+}
