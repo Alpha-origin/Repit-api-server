0b658406955ab8af6b46f02d9a5326860fe39ffb
kimwuju <ugim60350@gmail.com>
2026-06-04 16:40:02 +0900

    refacter: AnswerEntity userId JoinColumn to Column

--- src/main/java/repit/repit_api_server/domain/userdata/question/entity/Answer.java
+++ src/main/java/repit/repit_api_server/domain/userdata/question/entity/Answer.java
@@ -30,7 +30,7 @@ public class Answer {
     @OneToOne(fetch = FetchType.LAZY)
     private Question question;
 
-    @JoinColumn(nullable = false)
+    @Column(nullable = false)
     private Long userId;
 
     @Column(nullable = false)
