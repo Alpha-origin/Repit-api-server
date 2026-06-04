502d5504f7aba8428791beb2b3bf48d508f94870
kimwuju <ugim60350@gmail.com>
2026-06-04 17:26:04 +0900

    feat: AnswerResponse 어노테이션 추가

--- src/main/java/repit/repit_api_server/domain/userdata/question/dto/response/AnswerResponse.java
+++ src/main/java/repit/repit_api_server/domain/userdata/question/dto/response/AnswerResponse.java
@@ -1,10 +1,18 @@
 package repit.repit_api_server.domain.userdata.question.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import repit.repit_api_server.domain.userdata.interview.entity.Interview;
 import repit.repit_api_server.domain.userdata.question.entity.Question;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnswerResponse {
     private Long id;
 
