ea7bc39f17c148a07f7167bceef0c94ca4983582
kimwuju <ugim60350@gmail.com>
2026-06-04 21:42:36 +0900

    feat: getAllQuestion from QuestionController

--- src/main/java/repit/repit_api_server/domain/userdata/question/controller/QuestionController.java
+++ src/main/java/repit/repit_api_server/domain/userdata/question/controller/QuestionController.java
@@ -2,9 +2,12 @@ package repit.repit_api_server.domain.userdata.question.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import repit.repit_api_server.domain.userdata.interview.entity.Interview;
 import repit.repit_api_server.domain.userdata.question.dto.response.QuestionResponse;
 import repit.repit_api_server.domain.userdata.question.service.QuestionService;
 import repit.repit_api_server.global.common.ApiResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/interview")
@@ -22,5 +25,13 @@ public class QuestionController {
             @RequestHeader("Authorization") String authorization,
             @RequestParam Long questionId) {
         return ApiResponse.success(questionService.getQuestionById(authorization, questionId));
+    }
+
+    @GetMapping("/getAllQuestion")
+    public ApiResponse<List<QuestionResponse>> getAllQuestion(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody Interview interview
+    ) {
+        return ApiResponse.success(questionService.findAllByInterview(authorization, interview));
     }
 }
