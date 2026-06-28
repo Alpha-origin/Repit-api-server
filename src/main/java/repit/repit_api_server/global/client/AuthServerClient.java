dd053230148b97220f3c5e0905e210f9b6120a7a
kimwuju <ugim60350@gmail.com>
2026-06-28 19:52:19 +0900

    fix: 연결 중 오류 수정

--- src/main/java/repit/repit_api_server/global/client/AuthServerClient.java
+++ src/main/java/repit/repit_api_server/global/client/AuthServerClient.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import repit.repit_api_server.domain.metadata.dto.request.MetaDataRequest;
 import repit.repit_api_server.global.common.ApiResponse;
@@ -21,51 +22,69 @@ public class AuthServerClient {
 
     // Auth 서버에 metaData 전달
     public void createMetaData(String authorization, MetaDataRequest request) {
-        restClientBuilder
-                .baseUrl(authServerBaseUrl)
-                .build()
-                .post()
-                .uri("/api/v1/auth/createMetaData")
-                .header("Authorization", authorization)
-                .body(request)
-                .retrieve()
-                .toBodilessEntity();
+        try {
+            restClientBuilder
+                    .baseUrl(authServerBaseUrl)
+                    .build()
+                    .post()
+                    .uri("/api/v1/auth/createMetaData")
+                    .header("Authorization", authorization)
+                    .body(request)
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (HttpClientErrorException e) {
+            throw new RuntimeException("인증 실패");
+        }
+
     }
 
 
 
     // Auth 서버에서 metaData 반환
     public MetaDataResponse getMetaData(String authorization) {
-        ApiResponse<MetaDataResponse> response =
-                restClientBuilder
-                        .baseUrl(authServerBaseUrl)
-                        .build()
-                        .get()
-                        .uri("/api/v1/auth/getMetaData")
-                        .header("Authorization", authorization)
-                        .retrieve()
-                        .body(new ParameterizedTypeReference<ApiResponse<MetaDataResponse>>() {});
+        try {
+            ApiResponse<MetaDataResponse> response =
+                    restClientBuilder
+                            .baseUrl(authServerBaseUrl)
+                            .build()
+                            .get()
+                            .uri("/api/v1/auth/getMetaData")
+                            .header("Authorization", authorization)
+                            .retrieve()
+                            .body(new ParameterizedTypeReference<ApiResponse<MetaDataResponse>>() {
+                            });
 
-        if (response == null) {
-            return null;
+            if (response == null) {
+                return null;
+            }
+            return response.getData();
         }
-        return response.getData();
+        catch (HttpClientErrorException.Unauthorized e) {
+            throw new RuntimeException("인증 실패");
+        }
+
     }
 
     public UserResponse getUser(String authorization) {
-        ApiResponse<UserResponse> response =
-                restClientBuilder
-                        .baseUrl(authServerBaseUrl)
-                        .build()
-                        .get()
-                        .uri("/api/v1/users/me")
-                        .header("Authorization", authorization)
-                        .retrieve()
-                        .body(new ParameterizedTypeReference<ApiResponse<UserResponse>>() {});
+        try {
+            ApiResponse<UserResponse> response =
+                    restClientBuilder
+                            .baseUrl(authServerBaseUrl)
+                            .build()
+                            .get()
+                            .uri("/api/v1/users/me")
+                            .header("Authorization", authorization)
+                            .retrieve()
+                            .body(new ParameterizedTypeReference<ApiResponse<UserResponse>>() {});
 
-        if (response == null) {
-            return null;
+            if (response == null) {
+                return null;
+            }
+
+            return response.getData();
+
+        } catch (HttpClientErrorException.Unauthorized e) {
+            throw new RuntimeException("인증 실패");
         }
-        return response.getData();
     }
 }
