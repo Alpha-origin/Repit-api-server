53f04aec57078e39ec5e189b92ef8143997f5d89
kimwuju <ugim60350@gmail.com>
2026-07-06 17:09:43 +0900

    feat: 클라이언트에 완료 이벤트 보내는 emitter

--- src/main/java/repit/repit_api_server/domain/metadata/dto/response/CallbackSuccessResponse.java
+++ src/main/java/repit/repit_api_server/domain/metadata/dto/response/CallbackSuccessResponse.java
@@ -1,6 +1,5 @@
 package repit.repit_api_server.domain.metadata.dto.response;
 
-import io.swagger.v3.core.util.Json;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
